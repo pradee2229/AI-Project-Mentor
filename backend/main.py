207d5d97112a1bacd5c098362b59ee0d33793a79
pradee2229 <9124104113@kcgcollege.com>
2026-09-24 07:09:07 +0530

    Update CORS for Vercel

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+      "http://localhost:5173",
+      "https://frontend-k8om6e47u-casecraft.vercel.app"
+  ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
